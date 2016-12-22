983f6e0efe5100fc6ad1b8422e29eb4bd6c5f7c3
ExE Boss <ExE-Boss@users.noreply.github.com>
2016-12-22 10:25:45 +0100

    Begin work on Minecraft 1.11 wrappers

---
+++ src/main/java/nova/worldgen/WorldgenManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nova.worldgen;
+
+import nova.core.util.registry.Manager;
+
+/**
+ *
+ * @author ExE Boss
+ */
+public class WorldgenManager extends Manager<WorldgenManager> {
+
+	@Override
+	public void init() {
+		// TODO
+	}
+}
